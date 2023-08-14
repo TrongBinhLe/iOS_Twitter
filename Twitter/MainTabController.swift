009059cbe4b0b5afc387148f7411eb573a107ef0
TrongBinhLe <ltbinhbk2010@gmail.com>
2023-08-14 11:10:24 +0700

    Session 1: Building main uses interface 	1. Configure project and setup rootViewController.

--- Twitter/MainTabController.swift
+++ Twitter/MainTabController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainTabController.swift
 //  Twitter
 //
 //  Created by admin on 14/08/2023.
@@ -7,13 +7,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainTabController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        view.backgroundColor = .systemPink
     }
-
-
+    
 }
-
