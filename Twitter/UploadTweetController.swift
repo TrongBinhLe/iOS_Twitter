910c2086e4cd72dc26d43ab622c2e372cdf1bad5
TrongBinhLe <ltbinhbk2010@gmail.com>
2024-05-07 18:20:14 +0700

    Section 5: Continue with UI setup

--- Twitter/UploadTweetController.swift
+++ Twitter/UploadTweetController.swift
@@ -10,6 +10,8 @@ import UIKit
  
 class UploadTweetController: UIViewController {
     
+    private let user: User
+    
     // MARK: - Properties
     private lazy var actionButton: UIButton = {
         let button = UIButton(type: .system)
@@ -26,7 +28,26 @@ class UploadTweetController: UIViewController {
         
         return button
     }()
+    
+    private let profileImageView: UIImageView = {
+       let iv = UIImageView()
+        iv.contentMode = .scaleAspectFit
+        iv.clipsToBounds = true
+        iv.setDimensions(width: 48, height: 48)
+        iv.layer.cornerRadius = 48 / 2
+        
+        return iv
+    }()
     // MARK: - Lifecyle
+    
+    init(user: User) {
+        self.user = user
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -51,13 +72,21 @@ class UploadTweetController: UIViewController {
     
     func configure() {
         view.backgroundColor = .white
+        configureNavigationbar()
+        
+        view.addSubview(profileImageView)
+        profileImageView.anchor(top: view.safeAreaLayoutGuide.topAnchor, left: view.leftAnchor, paddingTop: 16, paddingLeft: 16)
+        
+        profileImageView.sd_setImage(with: user.profileImageUrl, completed: nil)
+        
+    }
+    
+    func configureNavigationbar() {
         navigationController?.navigationBar.barTintColor = .white
         navigationController?.navigationBar.isTranslucent = true
-    
         
         navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .cancel, target: self, action: #selector(handleCancel) )
         navigationItem.rightBarButtonItem = UIBarButtonItem(customView: actionButton)
-        
     }
     
 }
