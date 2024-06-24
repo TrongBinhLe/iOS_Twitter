293684065e841961984b991973526bb7b673fd66
TrongBinhLe <ltbinhbk2010@gmail.com>
2024-06-24 14:44:01 +0700

    Section 8: Search page Ui

--- Twitter/ExploreController.swift
+++ Twitter/ExploreController.swift
@@ -8,7 +8,9 @@
 import Foundation
 import UIKit
 
-class ExploreController: UIViewController {
+private let userIndentifier = "UserCell"
+
+class ExploreController: UITableViewController {
     
     //MARK: - Properties
     
@@ -23,9 +25,27 @@ class ExploreController: UIViewController {
     
     func configureUI() {
         view.backgroundColor = .white
-        
         navigationItem.title = "Explore"
+        
+        tableView.register(UserCell.self, forCellReuseIdentifier: userIndentifier)
+        tableView.separatorStyle = .none
         
     }
     
+}
+
+extension ExploreController {
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: userIndentifier, for: indexPath) as! UserCell
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 60
+    }
 }
